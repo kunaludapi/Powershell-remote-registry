bf98b575e5648f6192939165209a3f176919f1c0
Kunal Udapi <kunaludapi@gmail.com>
2018-02-14 16:24:48 +0530

    http://vcloud-lab.com
    get information from remote computers

--- Get-InstalledSoftwareInfo.ps1
+++ Get-InstalledSoftwareInfo.ps1
@@ -1,11 +1,41 @@
 function Get-InstalledSoftwareInfo {
+<#  
+  .Synopsis  
+    Get installed software information from remote computer.
+  .Description  
+    This cmdlet Get-InstalledSoftwareInfo will fetch and retrive information from remote and local computer. This requires remote registry service to be running. 
+  .Example  
+    Get-InstalledSoftwareInfo -ComputerName Server01
+     
+   This retrives list from computername server01
+  .Example
+    Get-InstalledSoftwareInfo -ComputerName Server01 | Export-CSV -Path c:\temp\info.csv
+    Get-InstalledSoftwareInfo -ComputerName Server01 | ft
+
+    Using pipeline information can be exported to CSV or shows tablewise
+  .OutPuts  
+    ComputerName DisplayName                                                    DisplayVersion Publisher             InstallDate EstimatedSize   
+    ------------ -----------                                                    -------------- ---------             ----------- --------
+    Server01     Microsoft Visual C++ 2012 x64 Additional Runtime - 11.0.61030  11.0.61030     Microsoft Corporation 20171225    5.82MB  
+    Server01     Microsoft Visual C++ 2008 Redistributable - x64 9.0.30729.6161 9.0.30729.6161 Microsoft Corporation 20170820    1.04MB 
+  .Notes  
+    NAME: Get-InstalledSoftwareInfo
+    AUTHOR: Kunal Udapi
+    CREATIONDATE: 02 January 2018
+    LASTEDIT: 7 January 2017  
+    KEYWORDS: Get installed software application information
+  .Link  
+   #Check Online version: http://kunaludapi.blogspot.com
+   #Check Online version: http://vcloud-lab.com
+   #Requires -Version 3.0  
+  #>
 [CmdletBinding(SupportsShouldProcess=$True,
     ConfirmImpact='Medium',
     HelpURI='http://vcloud-lab.com')]
     Param ( 
         [parameter(Position=0, ValueFromPipeline=$True, ValueFromPipelineByPropertyName=$True)]
         [alias('C')]
-        [String[]]$ComputerName = '.'
+        [String[]]$ComputerName = 'server01' #'.'
     )
     Begin {
     }
@@ -32,7 +62,7 @@ function Get-InstalledSoftwareInfo {
                         [Int]$rawsize = $Childsubkey.GetValue('EstimatedSize')
                         $ConvertedSize = $rawsize / 1024
                         $SoftwareSize = "{0:N2}MB" -f $ConvertedSize
-                        if ($Displayname -ne $null) {
+                        if ($SoftwareInfo -contains 'DisplayName') {
                             $SoftInfo = [PSCustomObject]@{
                                 ComputerName = $Computer
                                 DisplayName = $Childsubkey.GetValue('DisplayName')
@@ -45,10 +75,11 @@ function Get-InstalledSoftwareInfo {
                                 UninstallString = $Childsubkey.GetValue('UninstallString')
                                 RegistryLocation = $Childsubkey.Name
                             }
+                            $SoftInfo
                         }
-                        $SoftInfo 
-                        #break
+                        $Childsubkey.close()
                     }
+                $key.close()
                 }
             }
             else {
@@ -64,4 +95,4 @@ function Get-InstalledSoftwareInfo {
         #[Microsoft.Win32.RegistryHive]::CurrentConfig
     }
 }
-#Get-InstalledSoftwareInfo -ComputerName Server01, Member01
+Get-InstalledSoftwareInfo -ComputerName Server01, Member01 | ft
